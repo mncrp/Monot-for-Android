0618bfaab32e8ebf416731956759eca3ca6f83de
ZIP <56752821+1234Yosuke@users.noreply.github.com>
2022-04-17 22:16:13 +0900

    [Add] 検索エンジン変更機能
    他にもメニューのボタンにClickListenerつけたりしたよ(?)

--- app/src/main/java/com/example/monotforandroid/MainActivity.kt
+++ app/src/main/java/com/example/monotforandroid/MainActivity.kt
@@ -1,19 +1,17 @@
 package com.example.monotforandroid
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.media.Image
 import android.os.Bundle
-import android.util.Log
-import android.view.Gravity
 import android.view.KeyEvent
-import android.view.MotionEvent
 import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.view.isGone
-import androidx.core.view.isInvisible
+import androidx.preference.PreferenceManager
+
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("SetJavaScriptEnabled", "RtlHardcoded", "ClickableViewAccessibility")
@@ -28,6 +26,39 @@ class MainActivity : AppCompatActivity() {
         val menu = findViewById<LinearLayout>(R.id.menuLayout)
         val noMenu = findViewById<View>(R.id.noMenu)
         val kageZurashi = findViewById<View>(R.id.kageZurashi)
+        val settingButton = findViewById<Button>(R.id.settingsButton)
+        val backButton = findViewById<ImageButton>(R.id.backButton)
+        val forwardButton = findViewById<ImageButton>(R.id.forwardButton)
+        val reloadButton = findViewById<ImageButton>(R.id.reloadButton)
+        val homeButton = findViewById<ImageButton>(R.id.homeButton)
+
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        val search_engine = sharedPreferences.getString("search_engine", "")
+        var searchUrl_top = "null"
+        // 頭がおかしい(?)
+        when (search_engine) {
+            "Google" -> {
+                searchUrl_top = "https://www.google.com/search?q="
+            }
+            "Yahoo!" -> {
+                searchUrl_top = "https://search.yahoo.com/search?p="
+            }
+            "Yahoo! Japan" -> {
+                searchUrl_top = "https://search.yahoo.co.jp/search?p="
+            }
+            "DuckDuckGo" -> {
+                searchUrl_top = "https://duckduckgo.com/?q="
+            }
+            "Ecosia" -> {
+                searchUrl_top = "https://ecosia.org/search?q=a"
+            }
+            "Frea Search" -> {
+                searchUrl_top = "https://freasearch.org/search?q="
+            }
+            else -> {
+                searchUrl_top = "https://www.google.com/search?q="
+            }
+        }
 
         menu.visibility = View.INVISIBLE
         kageZurashi.visibility = View.INVISIBLE
@@ -66,12 +97,12 @@ class MainActivity : AppCompatActivity() {
                     editText.setText(urlText)
                 }
                 isItUrl != -1 -> {
-                    val searchUrl = "https://$urlText"
+                    val searchUrl = "$searchUrl_top$urlText"
                     webView.loadUrl(searchUrl)
                     editText.setText(searchUrl)
                 }
                 else -> {
-                    val searchUrl = "https://www.google.com/search?q=$urlText"
+                    val searchUrl = "$searchUrl_top$urlText"
                     webView.loadUrl(searchUrl)
                     editText.setText(searchUrl)
                 }
@@ -82,11 +113,28 @@ class MainActivity : AppCompatActivity() {
             Log.d("TAG", "Hi")*/
             menu.visibility = View.VISIBLE
             kageZurashi.visibility = View.VISIBLE
+            settingButton.setOnClickListener {
+                val intent = Intent(this, SettingsActivity::class.java)
+                startActivity(intent)
+            }
             noMenu.setOnClickListener {
                 menu.visibility = View.INVISIBLE
                 kageZurashi.visibility = View.INVISIBLE
                 noMenu.isClickable = false
             }
+        }
+
+        backButton.setOnClickListener {
+            webView.goBack()
+        }
+        forwardButton.setOnClickListener {
+            webView.goForward()
+        }
+        reloadButton.setOnClickListener {
+            webView.reload()
+        }
+        homeButton.setOnClickListener {
+            webView.loadUrl(searchUrl_top)
         }
     }
 
---
+++ app/src/main/java/com/example/monotforandroid/SettingsActivity.kt
@@ -0,0 +1,30 @@
+package com.example.monotforandroid
+
+import android.content.SharedPreferences
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.EditTextPreference
+import androidx.preference.PreferenceFragmentCompat
+
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.settings_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.settings, SettingsFragment())
+                .commit()
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    class SettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey)
+            val signaturePreference: EditTextPreference? = findPreference("search_engine")
+        }
+    }
+}
